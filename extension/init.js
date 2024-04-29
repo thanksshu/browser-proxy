fed6f9fa98a4149c7fb63faa801992c2be072a7f
hanshang <shangyuan.han@etu.utc.fr>
2024-04-29 08:18:43 +0200

    Use message channel

--- extension/init.js
+++ extension/init.js
@@ -7,7 +7,12 @@
          */
         fetch: async (request) => {
             const messageChannel = new MessageChannel();
-            
+            messageChannel.port1.addEventListener("message", () => {});
+            window.postMessage({
+                type: "__BROWSER_PROXY_EXTENSION_REQUEST__",
+                port: new MessageChannel(),
+                requestConfig: { url: "https://github.com/" },
+            });
         },
     };
 })();
