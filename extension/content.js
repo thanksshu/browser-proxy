e5a1af2bdb5e2885f579f640c1e38e885d7f430b
hanshang <shangyuan.han@etu.utc.fr>
2024-04-30 08:18:58 +0200

    Use dynamic import

--- extension/content.js
+++ extension/content.js
@@ -1,79 +1,81 @@
 "use strict";
 
-console.log(bufferToBase64);
-
-/**
- * Convert a request to request config
- * @param {Request} request
- * @returns {Object} Request Config
- */
-const requestToConfig = async (request) => {
-    return {
-        url: request.url,
-        headers: Object.entries(request.headers),
-        body: request.body,
-        cache: request.cache,
-        credentials: request.credentials,
-        integrity: request.integrity,
-        keepalive: request.keepalive,
-        method: request.method,
-        mode: request.mode,
-        redirect: request.redirect,
-        referrer: request.referrer,
-        referrerPolicy: request.referrerPolicy,
+import("./utils").then((utils) => {
+    /**
+     * Convert a request to request config
+     * @param {Request} request
+     * @returns {Object} Request Config
+     */
+    const requestToConfig = async (request) => {
+        return {
+            url: request.url,
+            headers: Object.entries(request.headers),
+            body: request.body,
+            cache: request.cache,
+            credentials: request.credentials,
+            integrity: request.integrity,
+            keepalive: request.keepalive,
+            method: request.method,
+            mode: request.mode,
+            redirect: request.redirect,
+            referrer: request.referrer,
+            referrerPolicy: request.referrerPolicy,
+        };
     };
-};
 
-/**
- * Send message received from background to page
- * @param {*} message
- * @param {MessagePort} port
- */
-const handleBackgroundMessage = (message, port) => {
-    if (message.type === "response") {
-        port.postMessage({
-            type: "__BROWSER_PROXY_EXTENSION_RESPONSE__",
-            response: message.response,
-        });
-    } else if (message.type === "error") {
-        port.postMessage(
-            buildErrorForPage({
-                type: "__BROWSER_PROXY_EXTENSION_ERROR__",
-                error: message.error,
-            })
-        );
-    }
-};
-
-window.addEventListener("message", (event) => {
-    const messageFromPage = event.data;
-
-    if (!Object.hasOwn(messageFromPage, "type")) {
-        return;
-    } else if (messageFromPage.type === "__BROWSER_PROXY_EXTENSION_REQUEST__") {
-        // A port is needed for each request
-        if (!Object.hasOwn(messageFromPage, "port")) {
-            return;
+    /**
+     * Send message received from background to page
+     * @param {*} message
+     * @param {MessagePort} port
+     */
+    const handleBackgroundMessage = (message, port) => {
+        if (message.type === "response") {
+            port.postMessage({
+                type: "__BROWSER_PROXY_EXTENSION_RESPONSE__",
+                response: message.response,
+            });
+        } else if (message.type === "error") {
+            port.postMessage(
+                buildErrorForPage({
+                    type: "__BROWSER_PROXY_EXTENSION_ERROR__",
+                    error: message.error,
+                })
+            );
         }
+    };
 
-        const request = messageFromPage.request;
-        const requestConfig = chrome.runtime.sendMessage(
-            // Send message to background, message must be JSON-ifiable
-            {
-                type: "request",
-                requestConfig,
-            },
-            // Handle message from background
-            (messageFromBackground) => {
-                handleBackgroundMessage(
-                    messageFromBackground,
-                    messageFromPage.port
-                );
+    window.addEventListener("message", (event) => {
+        const messageFromPage = event.data;
+
+        if (!Object.hasOwn(messageFromPage, "type")) {
+            return;
+        } else if (
+            messageFromPage.type === "__BROWSER_PROXY_EXTENSION_REQUEST__"
+        ) {
+            // A port is needed for each request
+            if (!Object.hasOwn(messageFromPage, "port")) {
+                return;
             }
-        );
-    }
-});
 
-console.info(
-    `${new Date().toLocaleString()} [Browser Proxy Extension] Content script loaded`
-);
+            const request = messageFromPage.request;
+            const requestConfig = chrome.runtime.sendMessage(
+                // Send message to background, message must be JSON-ifiable
+                {
+                    type: "request",
+                    requestConfig,
+                },
+                // Handle message from background
+                (messageFromBackground) => {
+                    handleBackgroundMessage(
+                        messageFromBackground,
+                        messageFromPage.port
+                    );
+                }
+            );
+        }
+    });
+
+    console.info(
+        `${new Date().toLocaleString()} [Browser Proxy Extension] Content script loaded`
+    );
+});
